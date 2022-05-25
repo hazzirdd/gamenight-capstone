6297884b9554dc573c34c5858e004e77bc709e76
Hayden Sweet <haydensweet22@gmail.com>
2022-05-25 15:58:01 -0600

    account features

--- client/account.js
+++ client/account.js
@@ -2,6 +2,8 @@ const usernameText = document.querySelector('.username-text');
 const passwordText = document.querySelector('.password-text');
 const loginBtn = document.querySelector('.login-btn');
 const signupBtn = document.querySelector('.signup-btn');
+const goToSignupBtn = document.querySelector('.go-to-signup-btn');
+const goToLoginBtn = document.querySelector('.go-to-login-btn');
 
 
 const loginCheck = () => {
@@ -9,14 +11,16 @@ const loginCheck = () => {
     let enteredUsername = usernameText.value
     let enteredPassword = passwordText.value 
     let login = false
+    console.log(`initial login = ${login}`)
 
     axios.get('/api/users')
     .then((res) => {
         res.data.forEach(user => {
             if (enteredUsername === user.username && enteredPassword === user.password) {
                 login = true
+                console.log(`User name matches! login = ${login}`)
                 window.location.replace('game-night.html')
-            }
+            } 
             
         })
     })
@@ -24,9 +28,14 @@ const loginCheck = () => {
         console.log(err)
     })
     
-    if (login === false) {
-        alert('incorrect username or password')
+    
+    const loginStatment = () => {
+        if (login === false) {
+            console.log(`log in failed loginStatment, it is ${login}`)
+            alert('incorrect username or password')
+        }
     }
+    setTimeout(loginStatment, 3000)
 }
 
 
@@ -51,7 +60,13 @@ const createUser = () => {
     window.location.replace('game-night.html')
 }
 
+const goToSignup = () => {
+    window.location.replace('signup.html')
+}
 
+const goToLogin = () => {
+    window.location.replace('login.html')
+}
 
 
 if (loginBtn !== null) {
@@ -60,4 +75,12 @@ if (loginBtn !== null) {
 
 if (signupBtn !== null) {
     signupBtn.addEventListener('click', createUser);
+}
+
+if (goToSignupBtn !== null) {
+    goToSignupBtn.addEventListener('click', goToSignup)
+}
+
+if (goToLoginBtn !== null) {
+    goToLoginBtn.addEventListener('click', goToLogin)
 }
